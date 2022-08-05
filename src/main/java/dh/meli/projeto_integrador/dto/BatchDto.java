2734b5c97c5c52ba04c90f59b5c9e2855076fc3e
amarinellif <amanda.marinelli@mercadolivre.com>
2022-08-05 11:29:32 -0300

    Feat: Add Batch Dto

--- src/main/java/dh/meli/projeto_integrador/dto/BatchDto.java
+++ src/main/java/dh/meli/projeto_integrador/dto/BatchDto.java
@@ -2,7 +2,8 @@ package dh.meli.projeto_integrador.dto;
 
 import dh.meli.projeto_integrador.model.Batch;
 import lombok.*;
-
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -11,14 +12,32 @@ import java.time.LocalTime;
 @AllArgsConstructor
 @Builder
 public class BatchDto {
+
     private long id;
+
+    @NotNull(message = "You should define product id.")
     private long productId;
+
+    @NotNull(message = "You should define current temperature.")
     private float currentTemperature;
+
+    @NotNull(message = "You should define minimum temperature.")
     private float minimumTemperature;
+
+    @NotNull(message = "You should define a valid quantity.")
+    @Min(value = 1)
     private int initialQuantity;
+
+    //TODO verificar se vamos utilizar a current quantity quando fizer o post
     private int currentQuantity;
+
+    @NotNull(message = "You should define due Date. Format: (yyyy-mm-dd).")
     private LocalDate manufacturingDate;
+
+    @NotNull(message = "You should define the manufacturing Time. Format: (hh:mm:ss).")
     private LocalTime manufacturingTime;
+
+    @NotNull(message = "You should define due Date. Format: (yyyy-mm-dd).")
     private LocalDate dueDate;
 
     public BatchDto(Batch batch) {
