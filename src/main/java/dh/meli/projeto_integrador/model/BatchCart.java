f4e034137a8f7eb095af322da00a747d5989ecd8
diovanavalim <diovana.valim@mercadolivre.com>
2022-08-05 10:51:24 -0300

    feat: add batch cart table entity

---
+++ src/main/java/dh/meli/projeto_integrador/model/BatchCart.java
@@ -0,0 +1,27 @@
+package dh.meli.projeto_integrador.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter @Setter
+@Entity
+@Table(name = "batch_cart")
+public class BatchCart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id", referencedColumnName = "id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "batch_id", referencedColumnName = "id", nullable = false)
+    private Batch batch;
+
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
+}
