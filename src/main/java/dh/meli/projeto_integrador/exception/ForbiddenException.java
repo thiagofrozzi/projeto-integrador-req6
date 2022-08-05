1622314aa775a41b4d8af0169dd6fb547b8bb830
diovanavalim <diovana.valim@mercadolivre.com>
2022-08-05 16:03:54 -0300

    feat: add forbidden exception

---
+++ src/main/java/dh/meli/projeto_integrador/exception/ForbiddenException.java
@@ -0,0 +1,11 @@
+package dh.meli.projeto_integrador.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class ForbiddenException extends RuntimeException {
+    public ForbiddenException(String message) {
+        super(message);
+    }
+}
