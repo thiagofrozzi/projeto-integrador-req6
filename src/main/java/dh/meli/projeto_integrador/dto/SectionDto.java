c6919491254c4743caae0d8760357586ccf9d946
diovanavalim <diovana.valim@mercadolivre.com>
2022-08-05 16:03:17 -0300

    feat: add section dto file

---
+++ src/main/java/dh/meli/projeto_integrador/dto/SectionDto.java
@@ -0,0 +1,14 @@
+package dh.meli.projeto_integrador.dto;
+
+import lombok.*;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SectionDto {
+
+    private long sectionId;
+
+    private long warehouseId;
+}
