03300751f70b00ef064a335b75fa793b6adfd77b
diovanavalim <diovana.valim@mercadolivre.com>
2022-08-05 16:02:24 -0300

    feat: add exception handler funcs

--- src/main/java/dh/meli/projeto_integrador/handler/ErrorHandler.java
+++ src/main/java/dh/meli/projeto_integrador/handler/ErrorHandler.java
@@ -1,4 +1,52 @@
 package dh.meli.projeto_integrador.handler;
 
+import dh.meli.projeto_integrador.exception.ExceptionDetails;
+import dh.meli.projeto_integrador.exception.ForbiddenException;
+import dh.meli.projeto_integrador.exception.InternalServerErrorException;
+import dh.meli.projeto_integrador.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
 public class ErrorHandler {
+
+    @ExceptionHandler(InternalServerErrorException.class)
+    public ResponseEntity<ExceptionDetails> internalServerErrorHandler(InternalServerErrorException e) {
+        return new ResponseEntity<ExceptionDetails>(ExceptionDetails
+                .builder()
+                .title("Internal Server Error")
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message(e.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build(),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<ExceptionDetails> forbiddenExceptionHandler(ForbiddenException e) {
+        return new ResponseEntity<ExceptionDetails>(ExceptionDetails
+                .builder()
+                .title("Forbidden Operation")
+                .status(HttpStatus.FORBIDDEN.value())
+                .message(e.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build(),
+                HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ExceptionDetails> resourceNotFoundExceptionHandler(ResourceNotFoundException e) {
+        return new ResponseEntity<ExceptionDetails>(ExceptionDetails
+                .builder()
+                .title("Resource Not Found")
+                .status(HttpStatus.NOT_FOUND.value())
+                .message(e.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build(),
+                HttpStatus.NOT_FOUND);
+    }
 }
