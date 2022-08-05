656677366e01ebad73c99c42ad76dc3080abe6e5
diovanavalim <diovana.valim@mercadolivre.com>
2022-08-05 15:59:31 -0300

    feat: add service layer

--- src/main/java/dh/meli/projeto_integrador/service/OrderService.java
+++ src/main/java/dh/meli/projeto_integrador/service/OrderService.java
@@ -1,3 +1,97 @@
 package dh.meli.projeto_integrador.service;
 
-public class OrderService {}
+import dh.meli.projeto_integrador.dto.BatchDto;
+import dh.meli.projeto_integrador.dto.OrderEntryDto;
+import dh.meli.projeto_integrador.exception.ForbiddenException;
+import dh.meli.projeto_integrador.exception.InternalServerErrorException;
+import dh.meli.projeto_integrador.model.*;
+import dh.meli.projeto_integrador.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private IOrderRepository orderRepository;
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private WarehouseService warehouseService;
+
+    @Autowired
+    private SectionService sectionService;
+
+    @Autowired
+    private AgentService agentService;
+
+    public Set<Batch> createInboundOrder(OrderEntryDto orderEntryDto) {
+        Warehouse warehouse = warehouseService.findWarehouse(orderEntryDto.getSection().getWarehouseId());
+
+        Section section = sectionService.findSection(orderEntryDto.getSection().getSectionId());
+
+        Agent agent = agentService.findAgent(orderEntryDto.getAgentId());
+
+        long agentWarehouseId = agent.getWarehouse().getId();
+
+        if (agentWarehouseId != warehouse.getId()) {
+            throw new ForbiddenException("Agent's warehouse ID does not belong to section's warehouse ID");
+        }
+
+        OrderEntry orderEntry = new OrderEntry();
+
+        orderEntry.setSection(orderEntry.getSection());
+        orderEntry.setOrderDate(orderEntryDto.getOrderDate());
+
+        Set<Batch> batches = new HashSet<Batch>();
+
+        int finalQuantity = 0;
+
+        for (BatchDto batchDto : orderEntryDto.getBatches()) {
+            Batch batch = new Batch();
+
+            Product product = productService.findProduct(batchDto.getProductId());
+
+            String productType = product.getSection().getProductType();
+
+            // Validates if product's section equals request given section
+            if (!productType.equals(section.getProductType())) {
+                throw new ForbiddenException(String.format("Product's %s section does not equals the given section",
+                        product.getName()));
+            }
+
+            batch.setCurrentTemperature(batchDto.getCurrentTemperature());
+            batch.setMinimumTemperature(batchDto.getMinimumTemperature());
+            batch.setInitialQuantity(batchDto.getInitialQuantity());
+            batch.setCurrentQuantity(batchDto.getCurrentQuantity());
+            batch.setManufacturingDate(batchDto.getManufacturingDate());
+            batch.setManufacturingTime(batchDto.getManufacturingTime());
+            batch.setDueDate(batchDto.getDueDate());
+            batch.setProduct(product);
+            batch.setOrderEntry(orderEntry);
+
+            batches.add(batch);
+            finalQuantity += batch.getInitialQuantity();
+        }
+
+        orderEntry.setBatches(batches);
+
+        // Validates if wanted section has available space for storing new products
+        if (finalQuantity > section.getMaxProductLoad()) {
+            throw new ForbiddenException("Product batches quantity sum overtakes section maximum product load");
+        }
+
+        try {
+            orderRepository.save(orderEntry);
+        } catch (Exception e) {
+            throw new InternalServerErrorException(e.getMessage());
+        }
+
+        return orderEntry.getBatches();
+    }
+}
---
+++ src/main/java/dh/meli/projeto_integrador/service/ProductService.java
@@ -0,0 +1,26 @@
+package dh.meli.projeto_integrador.service;
+
+import dh.meli.projeto_integrador.exception.ResourceNotFoundException;
+import dh.meli.projeto_integrador.model.Product;
+import dh.meli.projeto_integrador.repository.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private IProductRepository productRepository;
+
+    public Product findProduct(long id) {
+        Optional<Product> product = productRepository.findById(id);
+
+        if (product.isEmpty()) {
+            throw new ResourceNotFoundException(String.format("Could not find valid product for id %d", id));
+        }
+
+        return product.get();
+    }
+}
