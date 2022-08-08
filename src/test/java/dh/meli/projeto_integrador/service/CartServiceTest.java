f333f9f281f033598c5a540144383c6d1c266070
Thiago Frozzi <thiago.frozzi@mercadolivre.com>
2022-08-08 16:11:51 -0300

    feat: add arquivo inicio de arquivo de testes do service

--- src/test/java/dh/meli/projeto_integrador/service/CartServiceTest.java
+++ src/test/java/dh/meli/projeto_integrador/service/CartServiceTest.java
@@ -1,7 +1,37 @@
 package dh.meli.projeto_integrador.service;
 
+import dh.meli.projeto_integrador.repository.ICartRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class CartServiceTest {
 
+    @InjectMocks
+    CartService cartService;
+
+    @Mock
+    ICartRepository cartRepository;
+
+    @Test
+    void createCart() {
+    }
+
+    @Test
+    @DisplayName("Test update with success")
+    void updateStatusCartWithSuccess() {
+        BDDMockito.when(cartRepository.findById(ArgumentMatchers.anyLong()))
+                .thenReturn(TestUtilsGenerator.generateNewProperty());
+    }
 }
