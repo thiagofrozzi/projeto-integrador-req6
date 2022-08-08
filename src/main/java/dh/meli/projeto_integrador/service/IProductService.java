c5284791fe5ba88d646c3ba796d848417f265b33
racavalcante <rafael.cavalcante@mercadolivre.com>
2022-08-08 15:10:13 -0300

    docs: add java docs annotation

--- src/main/java/dh/meli/projeto_integrador/service/IProductService.java
+++ src/main/java/dh/meli/projeto_integrador/service/IProductService.java
@@ -5,6 +5,9 @@ import dh.meli.projeto_integrador.model.Product;
 
 import java.util.List;
 
+/**
+ * Interface to deal with the methods implemented on the ProductService class.
+ */
 public interface IProductService {
     List<ProductOutputDto> getAllProducts();
     List<ProductOutputDto> getProductsByCategory(String category);
