3a3cb41b1651d442b08ab26aee33f319726962e6
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-08-08 15:53:46 -0300

    doc: Inclusão de Java Docs no arquivo ICartRepository

--- src/main/java/dh/meli/projeto_integrador/repository/ICartRepository.java
+++ src/main/java/dh/meli/projeto_integrador/repository/ICartRepository.java
@@ -3,5 +3,11 @@ package dh.meli.projeto_integrador.repository;
 import dh.meli.projeto_integrador.model.Cart;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Interface ICartRepository will manage data persistence for Cart object instances.
+ * Will read, save, update and delete data through the GET, POST, PUT and DELETE requests.
+ * @author Gabriela Azevedo
+ * @version 0.0.1
+ */
 public interface ICartRepository extends CrudRepository<Cart, Long> {
 }
