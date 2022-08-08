d030091f06d94e35813f5bc193013e0f1ca37eb8
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-08-08 09:38:08 -0300

    feat: Alteração no modelo do carrinho

--- src/main/java/dh/meli/projeto_integrador/model/Cart.java
+++ src/main/java/dh/meli/projeto_integrador/model/Cart.java
@@ -1,15 +1,19 @@
 package dh.meli.projeto_integrador.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import dh.meli.projeto_integrador.enumClass.PurchaseOrderStatusEnum;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.Set;
 
 @Getter @Setter
 @Entity
 @Table(name = "cart")
+@Builder
 public class Cart {
 
     @Id
@@ -20,6 +24,11 @@ public class Cart {
     @JoinColumn(name = "customer_id", referencedColumnName = "id")
     @JsonIgnoreProperties("carts")
     private Customer customer;
+
+    @Enumerated(EnumType.STRING)
+    private PurchaseOrderStatusEnum status;
+
+    private LocalDate date;
 
     @OneToMany(mappedBy = "cart")
     @JsonIgnoreProperties("cart")
