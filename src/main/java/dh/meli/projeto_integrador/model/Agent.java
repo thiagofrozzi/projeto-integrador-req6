5f947dac41c2b141909319d626afd4433f5e3915
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-08-08 15:47:29 -0300

    doc: Inclusão de Java Docs no arquivo Agent

--- src/main/java/dh/meli/projeto_integrador/model/Agent.java
+++ src/main/java/dh/meli/projeto_integrador/model/Agent.java
@@ -6,9 +6,32 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
-@Getter @Setter
+/**
+ * Method Getter implemented by Lombok lib for get access the private attributes of the Agent Class
+ */
+@Getter
+
+/**
+ * Method Setter implemented by Lombok lib for set the private attributes of the Agent Class
+ */
+@Setter
+
+/**
+ * The @Entity annotation marks the Agent Class as an entity bean,
+ * so it must have a no-argument constructor that is visible at least with a protected scope.
+ */
 @Entity
+
+/**
+ * The @Table annotation is used to specify table details that will be used to persist our entities in the database.
+ */
 @Table(name = "agent")
+/**
+ * Class created for modeling the Agent entity on the database
+ * @author Diovana Valim
+ * @version 0.0.1
+ * @see java.lang.Object
+ */
 public class Agent {
 
     @Id
