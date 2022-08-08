bfcac9203c04d8eddd0781a4138e225b3666b0e0
Thiago Frozzi <thiago.frozzi@mercadolivre.com>
2022-08-08 14:35:50 -0300

    feat: add dto para retorno de sucesso no update do carrinho

---
+++ src/main/java/dh/meli/projeto_integrador/dtos/dtoOutput/UpdateStatusDto.java
@@ -0,0 +1,13 @@
+package dh.meli.projeto_integrador.dtos.dtoOutput;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UpdateStatusDto {
+    private String message;
+}
