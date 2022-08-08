55a1a28a5b5f777ba6df3c98083ba48043945a96
Thiago Frozzi <thiago.frozzi@mercadolivre.com>
2022-08-08 14:28:16 -0300

    feat: add rota de put de update do carrinho

--- src/main/java/dh/meli/projeto_integrador/controller/CartController.java
+++ src/main/java/dh/meli/projeto_integrador/controller/CartController.java
@@ -2,14 +2,12 @@ package dh.meli.projeto_integrador.controller;
 
 import dh.meli.projeto_integrador.dtos.dtoInput.CartDto;
 import dh.meli.projeto_integrador.dtos.dtoOutput.TotalPriceDto;
+import dh.meli.projeto_integrador.dtos.dtoOutput.UpdateStatusDto;
 import dh.meli.projeto_integrador.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -25,16 +23,13 @@ public class CartController {
         TotalPriceDto createdCart = cartService.createCart(cartDto);
         return new ResponseEntity<>(createdCart, HttpStatus.CREATED);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UpdateStatusDto> updatePurchaseOrder(@PathVariable Long id) {
+        UpdateStatusDto result = cartService.updateStatusCart(id);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 }
-
-
-
-
-
-
-
-
-
 
 
 
