353000a824b173d5caaec9829af10b6b47c8c0c7
Thiago Frozzi <thiago.frozzi@mercadolivre.com>
2022-08-08 14:29:11 -0300

    feat: add handler de erros referente ao update no carrinho

--- src/main/java/dh/meli/projeto_integrador/handler/ErrorHandler.java
+++ src/main/java/dh/meli/projeto_integrador/handler/ErrorHandler.java
@@ -1,4 +1,40 @@
 package dh.meli.projeto_integrador.handler;
 
+import dh.meli.projeto_integrador.exception.CartAlreadyFinishedException;
+import dh.meli.projeto_integrador.exception.CartNotFoundException;
+import dh.meli.projeto_integrador.exception.ExceptionDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
 public class ErrorHandler {
+    @ExceptionHandler(CartNotFoundException.class)
+    public ResponseEntity<ExceptionDetails> cartNotFoundHandler(CartNotFoundException exception) {
+
+        return new ResponseEntity<ExceptionDetails>(
+                ExceptionDetails.builder()
+                        .title("Cart Not Found")
+                        .message(exception.getMessage())
+                        .status(HttpStatus.NOT_FOUND.value())
+                        .timestamp(LocalDateTime.now())
+                        .build(),
+                HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(CartAlreadyFinishedException.class)
+    public ResponseEntity<ExceptionDetails> cartNotFoundHandler(CartAlreadyFinishedException exception) {
+
+        return new ResponseEntity<ExceptionDetails>(
+                ExceptionDetails.builder()
+                        .title("Cart Already Finished")
+                        .message(exception.getMessage())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .timestamp(LocalDateTime.now())
+                        .build(),
+                HttpStatus.NOT_FOUND);
+    }
 }
