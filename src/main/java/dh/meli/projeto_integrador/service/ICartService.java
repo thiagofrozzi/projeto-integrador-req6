c26ff8d2bdf305c6e2f3cb8f5f34e8a11fd3ddd8
Thiago Frozzi <thiago.frozzi@mercadolivre.com>
2022-08-08 14:31:53 -0300

    feat: add metodo de updateCart na interface

--- src/main/java/dh/meli/projeto_integrador/service/ICartService.java
+++ src/main/java/dh/meli/projeto_integrador/service/ICartService.java
@@ -2,8 +2,10 @@ package dh.meli.projeto_integrador.service;
 
 import dh.meli.projeto_integrador.dtos.dtoInput.CartDto;
 import dh.meli.projeto_integrador.dtos.dtoOutput.TotalPriceDto;
+import dh.meli.projeto_integrador.dtos.dtoOutput.UpdateStatusDto;
 
 public interface ICartService {
     // método para criação de um carrinho de compras
     TotalPriceDto createCart(CartDto cartDto);
+    UpdateStatusDto updateStatusCart(Long id);
 }
