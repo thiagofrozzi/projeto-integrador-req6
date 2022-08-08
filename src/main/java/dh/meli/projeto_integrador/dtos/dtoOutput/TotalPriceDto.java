97cbe1b1d65ee62edf1e0a6de1f01e2822386480
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-08-08 09:31:25 -0300

    feat: Criação da pasta de Dto de saída

---
+++ src/main/java/dh/meli/projeto_integrador/dtos/dtoOutput/TotalPriceDto.java
@@ -0,0 +1,12 @@
+package dh.meli.projeto_integrador.dtos.dtoOutput;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+public class TotalPriceDto {
+
+    private Double totalPrice;
+}
