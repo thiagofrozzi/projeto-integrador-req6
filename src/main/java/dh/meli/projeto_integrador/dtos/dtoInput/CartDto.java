b0012156261c9bc9dec903454d071c5c8be80a9c
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-08-08 09:30:16 -0300

    feat: Criação do CartDto

---
+++ src/main/java/dh/meli/projeto_integrador/dtos/dtoInput/CartDto.java
@@ -0,0 +1,29 @@
+package dh.meli.projeto_integrador.dtos.dtoInput;
+
+import dh.meli.projeto_integrador.enumClass.PurchaseOrderStatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter @Setter
+@AllArgsConstructor
+public class CartDto {
+
+    @NotNull(message = "The date of the creation cart has to be a date. Format: (yyyy-mm-dd)")
+    private LocalDate date;
+
+    private Long buyer_id;
+
+    @NotEmpty(message = "The status of the cart has to be OPENED of FINISHED")
+    private PurchaseOrderStatusEnum orderStatus;
+
+    @Size(min = 1, message = "The products list not to be empty")
+    private List< @Valid ProductDto> productList;
+}
