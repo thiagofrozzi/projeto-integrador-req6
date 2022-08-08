29ba0cf246bc8cf855092aaee8a3703e0eb3a5d9
Thiago Frozzi <thiago.frozzi@mercadolivre.com>
2022-08-08 19:33:23 -0300

    feat: implementa testes da rota de update no service

--- src/test/java/dh/meli/projeto_integrador/service/CartServiceTest.java
+++ src/test/java/dh/meli/projeto_integrador/service/CartServiceTest.java
@@ -1,6 +1,12 @@
 package dh.meli.projeto_integrador.service;
 
+import dh.meli.projeto_integrador.dtos.dtoOutput.UpdateStatusDto;
+import dh.meli.projeto_integrador.exception.CartNotFoundException;
+import dh.meli.projeto_integrador.model.Cart;
 import dh.meli.projeto_integrador.repository.ICartRepository;
+import dh.meli.projeto_integrador.utils.GenerateCart;
+import org.hibernate.sql.Update;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,8 +17,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -32,6 +42,25 @@ class CartServiceTest {
     @DisplayName("Test update with success")
     void updateStatusCartWithSuccess() {
         BDDMockito.when(cartRepository.findById(ArgumentMatchers.anyLong()))
-                .thenReturn(TestUtilsGenerator.generateNewProperty());
+                .thenReturn(Optional.of(GenerateCart.newCart1()));
+
+        UpdateStatusDto result = cartService.updateStatusCart(GenerateCart.newCart1().getId());
+
+        assertThat(result.getMessage()).isEqualTo("Cart Finished successfully");
+        verify(cartRepository, atLeastOnce()).findById(1L);
+
+    }
+
+
+    @Test
+    @DisplayName("Tests function when not found cart ")
+    void updateStatusCartNotFound() {
+
+        CartNotFoundException exception = Assertions.assertThrows(CartNotFoundException.class, () -> {
+            cartService.updateStatusCart(1L);
+        });
+
+        assertThat(exception.getMessage()).isEqualTo("Cart not found with this id");
+        verify(cartRepository, never()).save(GenerateCart.newCart1());
     }
 }
