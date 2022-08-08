41752631215b57f9a1336a42bcd79719b0e4a3f8
Thiago Frozzi <thiago.frozzi@mercadolivre.com>
2022-08-08 14:31:00 -0300

    feat: implementa metodo de update no service e na interface

--- src/main/java/dh/meli/projeto_integrador/service/CartService.java
+++ src/main/java/dh/meli/projeto_integrador/service/CartService.java
@@ -3,6 +3,10 @@ package dh.meli.projeto_integrador.service;
 import dh.meli.projeto_integrador.dtos.dtoInput.CartDto;
 import dh.meli.projeto_integrador.dtos.dtoInput.ProductDto;
 import dh.meli.projeto_integrador.dtos.dtoOutput.TotalPriceDto;
+import dh.meli.projeto_integrador.dtos.dtoOutput.UpdateStatusDto;
+import dh.meli.projeto_integrador.enumClass.PurchaseOrderStatusEnum;
+import dh.meli.projeto_integrador.exception.CartAlreadyFinishedException;
+import dh.meli.projeto_integrador.exception.CartNotFoundException;
 import dh.meli.projeto_integrador.model.*;
 import dh.meli.projeto_integrador.repository.IBatchCartRepository;
 import dh.meli.projeto_integrador.repository.IBatchRepository;
@@ -14,6 +18,7 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CartService implements ICartService {
@@ -71,5 +76,17 @@ public class CartService implements ICartService {
         List<ProductDto> productsList = cartDto.getProductList();
         buildBatchCart(savedCart, productsList);
         return totalCartPrice(productsList);
+    }
+
+    public UpdateStatusDto updateStatusCart(Long id){
+        Cart existCart = cartRepository.findById(id).orElseThrow(() -> new CartNotFoundException("Cart not found with this id"));
+
+        if(existCart.getStatus() == PurchaseOrderStatusEnum.FINISHED) throw new CartAlreadyFinishedException("Cart already Finished");
+
+        existCart.setStatus(PurchaseOrderStatusEnum.FINISHED);
+
+        cartRepository.save(existCart);
+
+        return new UpdateStatusDto("Cart Finished successfully");
     }
 }
