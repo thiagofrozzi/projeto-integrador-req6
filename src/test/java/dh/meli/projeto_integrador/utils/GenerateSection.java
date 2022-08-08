f36d152b83f286d3440c08b96338ed5c796a30eb
Thiago Frozzi <thiago.frozzi@mercadolivre.com>
2022-08-08 19:30:36 -0300

    feat: cria arquivos utils para gerar objetos pros testes

---
+++ src/test/java/dh/meli/projeto_integrador/utils/GenerateSection.java
@@ -0,0 +1,29 @@
+package dh.meli.projeto_integrador.utils;
+
+import dh.meli.projeto_integrador.model.BatchCart;
+import dh.meli.projeto_integrador.model.OrderEntry;
+import dh.meli.projeto_integrador.model.Product;
+import dh.meli.projeto_integrador.model.Section;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class GenerateSection {
+//    public static Section newSection1() {
+//        return Section.builder()
+//                .id(1)
+//                .warehouse(GenerateWarehouse.newWareHouse1())
+//                .productType("Fresco")
+//                .maxProductLoad(100L)
+//                .currentProductLoad(100L)
+//                .orderEntries(GenerateOrderEntry.newListOrderEntry1())
+//                .build();
+//    }
+//
+//    public static Set<Section> newListSection() {
+//        Set<Section> listSection =  new HashSet<>();
+//        listSection.add(GenerateSection.newSection1());
+//
+//        return listSection;
+//    }
+}
---
+++ src/test/java/dh/meli/projeto_integrador/utils/GenerateWarehouse.java
@@ -0,0 +1,21 @@
+package dh.meli.projeto_integrador.utils;
+
+import dh.meli.projeto_integrador.model.OrderEntry;
+import dh.meli.projeto_integrador.model.Product;
+import dh.meli.projeto_integrador.model.Section;
+import dh.meli.projeto_integrador.model.Warehouse;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class GenerateWarehouse {
+//    public static Warehouse newWareHouse1() {
+//        return Warehouse.builder()
+//                .id(1)
+//                .sections(GenerateSection.newListSection())
+//                .agents(GenerateAgents.newListAgent())
+//                .name("Armazem A")
+//                .address("Rua Almeida 259")
+//                .build();
+//    }
+}
