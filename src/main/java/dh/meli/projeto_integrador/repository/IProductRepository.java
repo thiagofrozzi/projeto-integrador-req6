a8b05c8dbff038f612f52974830aa7b92e52b13e
racavalcante <rafael.cavalcante@mercadolivre.com>
2022-08-08 09:44:40 -0300

    feature: add an method to filter products based on their type/category

--- src/main/java/dh/meli/projeto_integrador/repository/IProductRepository.java
+++ src/main/java/dh/meli/projeto_integrador/repository/IProductRepository.java
@@ -2,5 +2,11 @@ package dh.meli.projeto_integrador.repository;
 
 import dh.meli.projeto_integrador.model.Product;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface IProductRepository extends CrudRepository<Product, Long> {}
+import java.util.List;
+
+@Repository
+public interface IProductRepository extends CrudRepository<Product, Long> {
+    List<Product> findAllByType(String type);
+}
