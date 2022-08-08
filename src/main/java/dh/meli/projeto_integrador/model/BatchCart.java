49acb00d07c0a7638feaccdee48369f6cc8c8701
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-08-08 15:48:46 -0300

    doc: Inclusão de Java Docs Batch Cart

--- src/main/java/dh/meli/projeto_integrador/model/BatchCart.java
+++ src/main/java/dh/meli/projeto_integrador/model/BatchCart.java
@@ -1,15 +1,46 @@
 package dh.meli.projeto_integrador.model;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
-@Getter @Setter
+/**
+ * Method Getter implemented by Lombok lib for get access the private attributes of the BatchCart Class
+ */
+@Getter
+
+/**
+ * Method Setter implemented by Lombok lib for set access the private attributes of the BatchCart Class
+ */
+@Setter
+
+/**
+ * The @Entity annotation marks the BatchCart Class as an entity bean,
+ * so it must have a no-argument constructor that is visible at least with a protected scope.
+ */
 @Entity
+
+/**
+ * The @Table annotation is used to specify table details that will be used to persist our entities in the database.
+ */
 @Table(name = "batch_cart")
+
+/**
+ * Method Builder implemented by Lombok lib
+ */
 @Builder
+
+/**
+ * Method Constructor with all arguments implemented by Lombok lib
+ */
+@AllArgsConstructor
+
+/**
+ * Class created for modeling the BatchCart entity on the database
+ * @author Diovana Valim
+ * @version 0.0.1
+ * @see java.lang.Object
+ */
 public class BatchCart {
 
     @Id
@@ -26,4 +57,8 @@ public class BatchCart {
 
     @Column(name = "quantity", nullable = false)
     private int quantity;
+
+    public BatchCart() {
+
+    }
 }
