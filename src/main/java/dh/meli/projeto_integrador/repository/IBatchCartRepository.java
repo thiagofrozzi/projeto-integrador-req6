c16528f468c7ac1293c55a677ce51993e7ad9a6a
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-08-08 09:32:42 -0300

    feat: Criação da interface BatchCart repository

---
+++ src/main/java/dh/meli/projeto_integrador/repository/IBatchCartRepository.java
@@ -0,0 +1,7 @@
+package dh.meli.projeto_integrador.repository;
+
+import dh.meli.projeto_integrador.model.BatchCart;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IBatchCartRepository extends CrudRepository<BatchCart, Long> {
+}
