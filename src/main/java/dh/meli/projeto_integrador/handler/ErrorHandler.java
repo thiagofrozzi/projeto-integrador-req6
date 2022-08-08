56c381c9d77a3de8e793f5c21d28aa9235df3d6f
racavalcante <rafael.cavalcante@mercadolivre.com>
2022-08-08 15:07:15 -0300

    docs: add java docs annotation

--- src/main/java/dh/meli/projeto_integrador/handler/ErrorHandler.java
+++ src/main/java/dh/meli/projeto_integrador/handler/ErrorHandler.java
@@ -9,9 +9,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
 
+/**
+ * Class that handle the exceptions that are thrown over the code execution flow.
+ * @author Diovana Valim, Rafael Cavalcante
+ * @version 0.0.1
+ */
 @ControllerAdvice
 public class ErrorHandler {
 
+    /**
+     * Method that captures a NotFoundException and build a response to send through HTTP request.
+     * @param ex instance of NotFoundException class captured during the code execution flow.
+     * @return a ResponseEntity containing details of the exception and a compatible HTTP status code.
+     */
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ExceptionDetails> handlerNotFoundEx(NotFoundException ex) {
         return new ResponseEntity<>(
