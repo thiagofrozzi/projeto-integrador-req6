49b81db25c17f09b47b2b8897236d1b686a87eb6
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-08-08 15:55:29 -0300

    doc: Inclusão de Java Docs no arquivo ICartService

--- src/main/java/dh/meli/projeto_integrador/service/ICartService.java
+++ src/main/java/dh/meli/projeto_integrador/service/ICartService.java
@@ -3,6 +3,12 @@ package dh.meli.projeto_integrador.service;
 import dh.meli.projeto_integrador.dtos.dtoInput.CartDto;
 import dh.meli.projeto_integrador.dtos.dtoOutput.TotalPriceDto;
 
+/**
+ * Interface ICartService will manage data persistence for Cart object instances.
+ * Will read, save, update and delete data through the GET, POST, PUT and DELETE requests.
+ * @author Gabriela Azevedo
+ * @version 0.0.1
+ */
 public interface ICartService {
     // método para criação de um carrinho de compras
     TotalPriceDto createCart(CartDto cartDto);
