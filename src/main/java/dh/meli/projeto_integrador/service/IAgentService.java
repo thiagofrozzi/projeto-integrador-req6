6147508f6752711a481ea95a5e7bd34118e52fc2
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-08-09 14:39:17 -0300

    doc: Inclusão de documentação

--- src/main/java/dh/meli/projeto_integrador/service/IAgentService.java
+++ src/main/java/dh/meli/projeto_integrador/service/IAgentService.java
@@ -8,5 +8,11 @@ import dh.meli.projeto_integrador.model.Agent;
  * @version 0.0.1
  */
 public interface IAgentService {
+
+    /**
+     * Method implemented by AgentService for to find an Agent by Id
+     * @param id long coming from user request
+     * @return an object of type Agent
+     */
     Agent findAgent(long id);
 }
