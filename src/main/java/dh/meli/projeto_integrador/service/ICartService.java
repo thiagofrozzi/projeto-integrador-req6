233b9059fdf31a0930d3e851ceafafbadd7257a6
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-08-09 15:07:47 -0300

    doc: inclusão de documentação

--- src/main/java/dh/meli/projeto_integrador/service/ICartService.java
+++ src/main/java/dh/meli/projeto_integrador/service/ICartService.java
@@ -11,7 +11,7 @@ import dh.meli.projeto_integrador.dto.dtoOutput.TotalPriceDto;
  */
 public interface ICartService {
     /**
-     * Method implemented by CartService for to create a Cart
+     * Method for to create a Cart
      * @param cartDto an object of type CartDto coming from user request
      * @return an object of type TotalPriceDto
      */
