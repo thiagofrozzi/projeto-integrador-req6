b44de547334f197a2690dc1ea45655df31827987
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-08-09 14:41:42 -0300

    doc: inclusão de documentação

--- src/main/java/dh/meli/projeto_integrador/service/IWarehouseService.java
+++ src/main/java/dh/meli/projeto_integrador/service/IWarehouseService.java
@@ -2,6 +2,16 @@ package dh.meli.projeto_integrador.service;
 
 import dh.meli.projeto_integrador.model.Warehouse;
 
+/**
+ * Interface to specify service methods implemented on OrderService class.
+ * @author Diovana Valim, Thiago Guimaraes;
+ * @version 0.0.1
+ */
 public interface IWarehouseService {
+    /**
+     *
+     * @param id
+     * @return
+     */
     Warehouse findWarehouse(long id);
 }
