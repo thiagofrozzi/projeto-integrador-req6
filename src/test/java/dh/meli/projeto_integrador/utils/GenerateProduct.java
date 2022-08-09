aaa31fc945ef7ce79463d214dba7486756386a5b
Thiago Frozzi <thiago.frozzi@mercadolivre.com>
2022-08-09 14:54:37 -0300

    feat: unifica arquivo util Product

--- src/test/java/dh/meli/projeto_integrador/utils/GenerateProduct.java
+++ src/test/java/dh/meli/projeto_integrador/utils/GenerateProduct.java
@@ -1,20 +1,44 @@
 package dh.meli.projeto_integrador.utils;
 
+import dh.meli.projeto_integrador.dtos.dtoInput.ProductDto;
 import dh.meli.projeto_integrador.enumClass.PurchaseOrderStatusEnum;
 import dh.meli.projeto_integrador.model.Batch;
 import dh.meli.projeto_integrador.model.Cart;
 import dh.meli.projeto_integrador.model.Product;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GenerateProduct {
-//    public static Product newProduct1() {
-//        return Product.builder()
-//                .id(1)
-//                .name("Morango")
-//                .type("Fresco")
-//                .price(4.99)
+    public static Product newProduct1() {
+        return Product.builder()
+                .id(1)
+                .name("Morango")
+                .type("Fresco")
+                .price(4.99)
 //                .batches(GenerateBatch.newListBatch1())
-//                .build();
-//    }
+                .build();
+    }
+
+public static ProductDto newProductDto() {
+    return ProductDto.builder()
+            .batchId(1L)
+            .quantity(10)
+            .build();
+}
+
+    public static List<Product> newProductList(){
+        List<Product> products = new ArrayList<>();
+        products.add(newProduct1());
+
+        return products;
+    }
+
+    public static List<ProductDto> newProductListDto(){
+        List<ProductDto> products = new ArrayList<>();
+        products.add(newProductDto());
+
+        return products;
+    }
 }
