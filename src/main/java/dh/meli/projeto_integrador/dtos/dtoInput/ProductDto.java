ff3262dc3fb1887db008d90867a9c501ab4eacb2
Thiago Frozzi <thiago.frozzi@mercadolivre.com>
2022-08-09 14:53:43 -0300

    feat: add builder no ProductDto

--- src/main/java/dh/meli/projeto_integrador/dtos/dtoInput/ProductDto.java
+++ src/main/java/dh/meli/projeto_integrador/dtos/dtoInput/ProductDto.java
@@ -1,6 +1,7 @@
 package dh.meli.projeto_integrador.dtos.dtoInput;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,6 +10,7 @@ import javax.validation.constraints.NotNull;
 
 @Getter @Setter
 @AllArgsConstructor
+@Builder
 public class ProductDto {
 
     @NotNull(message = "The product must have an ID number")
