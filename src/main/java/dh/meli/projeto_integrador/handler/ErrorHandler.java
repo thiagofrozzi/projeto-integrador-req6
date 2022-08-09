0ab29e9768a81ef1d2f87cfa8490569c097ff549
diovanavalim <diovana.valim@mercadolivre.com>
2022-08-09 09:57:31 -0300

    rebase

--- src/main/java/dh/meli/projeto_integrador/handler/ErrorHandler.java
+++ src/main/java/dh/meli/projeto_integrador/handler/ErrorHandler.java
@@ -2,6 +2,9 @@ package dh.meli.projeto_integrador.handler;
 
 import dh.meli.projeto_integrador.exception.ExceptionDetails;
 import dh.meli.projeto_integrador.exception.NotFoundException;
+import dh.meli.projeto_integrador.exception.ForbiddenException;
+import dh.meli.projeto_integrador.exception.InternalServerErrorException;
+import dh.meli.projeto_integrador.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -34,4 +37,55 @@ public class ErrorHandler {
                 HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * Method that captures a InternalServerErrorException and build a response to send through HTTP request.
+     * @param ex instance of InternalServerErrorException class captured during the code execution flow.
+     * @return a ResponseEntity containing details of the exception and a compatible HTTP status code.
+     */
+    @ExceptionHandler(InternalServerErrorException.class)
+    public ResponseEntity<ExceptionDetails> internalServerErrorHandler(InternalServerErrorException e) {
+        return new ResponseEntity<ExceptionDetails>(ExceptionDetails
+                .builder()
+                .title("Internal Server Error")
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message(e.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build(),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * Method that captures a ForbiddenException and build a response to send through HTTP request.
+     * @param ex instance of ForbiddenException class captured during the code execution flow.
+     * @return a ResponseEntity containing details of the exception and a compatible HTTP status code.
+     */
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<ExceptionDetails> forbiddenExceptionHandler(ForbiddenException e) {
+        return new ResponseEntity<ExceptionDetails>(ExceptionDetails
+                .builder()
+                .title("Forbidden Operation")
+                .status(HttpStatus.FORBIDDEN.value())
+                .message(e.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build(),
+                HttpStatus.FORBIDDEN);
+    }
+
+    /**
+     * Method that captures a ResourceNotFoundException and build a response to send through HTTP request.
+     * @param ex instance of ResourceNotFoundException class captured during the code execution flow.
+     * @return a ResponseEntity containing details of the exception and a compatible HTTP status code.
+     */
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ExceptionDetails> resourceNotFoundExceptionHandler(ResourceNotFoundException e) {
+        return new ResponseEntity<ExceptionDetails>(ExceptionDetails
+                .builder()
+                .title("Resource Not Found")
+                .status(HttpStatus.NOT_FOUND.value())
+                .message(e.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build(),
+                HttpStatus.NOT_FOUND);
+    }
+    
 }
