bb956f6fba34f41e0b06db42c16a67a4253ca578
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-08-09 15:08:55 -0300

    doc: inclusão de documentação

--- src/main/java/dh/meli/projeto_integrador/service/ISectionService.java
+++ src/main/java/dh/meli/projeto_integrador/service/ISectionService.java
@@ -3,15 +3,15 @@ package dh.meli.projeto_integrador.service;
 import dh.meli.projeto_integrador.model.Section;
 
 /**
- * Interface to specify service methods implemented on OrderService class.
+ * Interface to specify service methods implemented on SectionService class.
  * @author Diovana Valim, Thiago Guimaraes;
  * @version 0.0.1
  */
 public interface ISectionService {
     /**
-     *
-     * @param id
-     * @return
+     * Method for to find a Section by Id
+     * @param id long
+     * @return an object of type Section
      */
     Section findSection(long id);
 }
