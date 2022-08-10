5041de02ed09ea5f4366e467ce75280bb8ddae24
racavalcante <rafael.cavalcante@mercadolivre.com>
2022-08-10 09:33:27 -0300

    feat: add a get route to request a cart by id

--- src/main/java/dh/meli/projeto_integrador/controller/CartController.java
+++ src/main/java/dh/meli/projeto_integrador/controller/CartController.java
@@ -1,17 +1,17 @@
 package dh.meli.projeto_integrador.controller;
 
 import dh.meli.projeto_integrador.dto.dtoInput.CartDto;
+import dh.meli.projeto_integrador.dto.dtoOutput.CartOutputDto;
 import dh.meli.projeto_integrador.dto.dtoOutput.TotalPriceDto;
+import dh.meli.projeto_integrador.model.Cart;
 import dh.meli.projeto_integrador.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 /**
  * Class responsible for processing user's requests and generating appropriated HTTP responses;
@@ -37,6 +37,11 @@ public class CartController {
     public ResponseEntity<TotalPriceDto> createNewPurchaseOrder(@RequestBody @Valid CartDto cartDto) {
         TotalPriceDto createdCart = cartService.createCart(cartDto);
         return new ResponseEntity<>(createdCart, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/orders/{id}")
+    public ResponseEntity<CartOutputDto> getCartById(@PathVariable Long id) {
+        return ResponseEntity.ok(cartService.getCartById(id));
     }
 }
 
