9a29807e75f21843faf39f96ced96fed7dbb595c
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-08-10 18:59:38 -0300

    test: criação dos testes de unidade da camada de serviço

--- src/test/java/dh/meli/projeto_integrador/service/CartServiceTest.java
+++ src/test/java/dh/meli/projeto_integrador/service/CartServiceTest.java
@@ -3,12 +3,11 @@ package dh.meli.projeto_integrador.service;
 import dh.meli.projeto_integrador.dto.dtoInput.CartDto;
 import dh.meli.projeto_integrador.dto.dtoOutput.TotalPriceDto;
 import dh.meli.projeto_integrador.dto.dtoOutput.UpdateStatusDto;
-import dh.meli.projeto_integrador.exception.CartNotFoundException;
+import dh.meli.projeto_integrador.exception.ForbiddenException;
+import dh.meli.projeto_integrador.exception.ResourceNotFoundException;
 import dh.meli.projeto_integrador.model.*;
 import dh.meli.projeto_integrador.repository.*;
-import dh.meli.projeto_integrador.util.Generators;
 import dh.meli.projeto_integrador.utils.*;
-import org.hibernate.sql.Update;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,7 +23,6 @@ import static org.mockito.Mockito.*;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
@@ -79,28 +77,66 @@ class CartServiceTest {
     }
 
     @Test
+    void create_returnException_whenQuantityIsNotValid() {
+        Cart newCartWithId = GenerateCart.newCartWithId1();
+        Customer newCustomer = GenerateCustomer.newCustomer1();
+        Product newProduct = GenerateProduct.newProduct1();
+        Batch newBatch = GenerateBatch.newBatchInvalidQuantity();
+        CartDto cartDto = GenerateCartDto.newCartDto();
+
+        BDDMockito.when(customerRepository.findById(ArgumentMatchers.anyLong()))
+            .thenReturn(Optional.of(newCustomer));
+        BDDMockito.when(cartRepository.save(ArgumentMatchers.any(Cart.class)))
+                .thenReturn(newCartWithId);
+        BDDMockito.when(productRepository.findById(ArgumentMatchers.anyLong()))
+                .thenReturn(Optional.of(newProduct));
+        BDDMockito.when(batchRepository.findByProduct(ArgumentMatchers.any(Product.class)))
+                .thenReturn(newBatch);
+
+        ForbiddenException exception = Assertions.assertThrows(ForbiddenException.class, () -> {
+            cartService.createCart(cartDto);
+        });
+
+        assertThat(exception.getMessage()).isEqualTo("The product: [Morango] does not have enough quantity in stock.");
+        verify(cartRepository, never()).save(GenerateCart.newCart1());
+    }
+
+    @Test
     @DisplayName("Test update with success")
     void updateStatusCartWithSuccess() {
         BDDMockito.when(cartRepository.findById(ArgumentMatchers.anyLong()))
-                .thenReturn(Optional.of(GenerateCart.newCart1()));
+                .thenReturn(Optional.of(GenerateCart.newCartWithId1()));
 
-        UpdateStatusDto result = cartService.updateStatusCart(GenerateCart.newCart1().getId());
+        UpdateStatusDto result = cartService.updateStatusCart(GenerateCart.newCartWithId1().getId());
 
         assertThat(result.getMessage()).isEqualTo("Cart Finished successfully");
         verify(cartRepository, atLeastOnce()).findById(1L);
-
     }
 
 
     @Test
-    @DisplayName("Tests function when not found cart ")
+    @DisplayName("Tests function when not found cart")
     void updateStatusCartNotFound() {
 
-        CartNotFoundException exception = Assertions.assertThrows(CartNotFoundException.class, () -> {
+        ResourceNotFoundException exception = Assertions.assertThrows(ResourceNotFoundException.class, () -> {
             cartService.updateStatusCart(1L);
         });
 
         assertThat(exception.getMessage()).isEqualTo("Cart not found with this id");
+        verify(cartRepository, never()).save(GenerateCart.newCart1());
+    }
+
+    @Test
+    @DisplayName("Tests exception when cart status is already finished")
+    void update_returnException_WhenCartStatusAlreadyFinished() {
+        BDDMockito.when(cartRepository.findById(ArgumentMatchers.anyLong()))
+                .thenReturn(Optional.of(GenerateCart.newCartWithId2()));
+
+            ForbiddenException exception = Assertions.assertThrows(ForbiddenException.class, () -> {
+                cartService.updateStatusCart(1L);
+        });
+
+        assertThat(exception.getMessage()).isEqualTo("Cart already Finished");
         verify(cartRepository, never()).save(GenerateCart.newCart1());
     }
 }
