8b0f8719b5190f1f790a9aa3f67d56e7b27d7c6f
racavalcante <rafael.cavalcante@mercadolivre.com>
2022-08-10 09:34:57 -0300

    feat: add a method to handle a get request and ohter method to auxiliate it

--- src/main/java/dh/meli/projeto_integrador/service/CartService.java
+++ src/main/java/dh/meli/projeto_integrador/service/CartService.java
@@ -2,18 +2,23 @@ package dh.meli.projeto_integrador.service;
 
 import dh.meli.projeto_integrador.dto.dtoInput.CartDto;
 import dh.meli.projeto_integrador.dto.dtoInput.ProductDto;
+import dh.meli.projeto_integrador.dto.dtoOutput.CartOutputDto;
+import dh.meli.projeto_integrador.dto.dtoOutput.CartProductsOutputDto;
 import dh.meli.projeto_integrador.dto.dtoOutput.TotalPriceDto;
+import dh.meli.projeto_integrador.exception.ResourceNotFoundException;
 import dh.meli.projeto_integrador.model.*;
-import dh.meli.projeto_integrador.repository.IProductCartRepository;
-import dh.meli.projeto_integrador.repository.IBatchRepository;
-import dh.meli.projeto_integrador.repository.ICartRepository;
-import dh.meli.projeto_integrador.repository.ICustomerRepository;
+import dh.meli.projeto_integrador.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+
+
 
 /**
  * Class responsible for business rules and communication with the Cart Repository layer
@@ -46,6 +51,13 @@ public class CartService implements ICartService {
      */
     @Autowired
     private ICustomerRepository customerRepository;
+
+
+    /**
+     * Dependency Injection of the Product Repository.
+     */
+    @Autowired
+    private IProductRepository productRepository;
 
     /**
      * Method that receives an object of type CartDto, build the cart object and saves on the Cart table.
@@ -108,5 +120,49 @@ public class CartService implements ICartService {
         List<ProductDto> productsList = cartDto.getProducts();
         buildBatchCart(savedCart, productsList);
         return totalCartPrice(productsList);
+    }
+
+    private List<CartProductsOutputDto> createCartProductList (List<ProductCart> cartProducts) {
+        List<CartProductsOutputDto> cartProductsDtos = new ArrayList<>();
+        for (ProductCart productCart : cartProducts) {
+            Product product = productRepository.findById(productCart.getProduct().getId()).get();
+            cartProductsDtos.add(CartProductsOutputDto.builder()
+                    .name(product.getName())
+                    .type(product.getType())
+                    .price(product.getPrice())
+                    .quantity(productCart.getQuantity())
+                    .subtotal(product.getPrice() * productCart.getQuantity())
+                    .build()
+            );
+        }
+        return cartProductsDtos;
+    }
+
+    private Double calculateCartTotal(List<CartProductsOutputDto> cartProductsDtos){
+        Double total = 0.0;
+        for (CartProductsOutputDto productCartDto : cartProductsDtos) {
+            total += productCartDto.getSubtotal();
+        }
+        return total;
+    }
+    public CartOutputDto getCartById(Long id) {
+        Optional<Cart> cart = cartRepository.findById(id);
+
+        if(cart.isEmpty()) throw new ResourceNotFoundException(String.format("Could not find valid cart for id %d", id));
+
+        Customer customer = customerRepository.findById(cart.get().getCustomer().getId()).get();
+
+        List<ProductCart> cartProducts = new ArrayList<>(cart.get().getProductCarts());
+
+        List<CartProductsOutputDto> cartProductsDtos = createCartProductList(cartProducts);
+
+        Double total = calculateCartTotal(cartProductsDtos);
+        return CartOutputDto.builder()
+                .customerName(customer.getName())
+                .status(cart.get().getStatus())
+                .date(cart.get().getDate())
+                .products(cartProductsDtos)
+                .total(total)
+                .build();
     }
 }
