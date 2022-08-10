fd12617870f8ebe31311b26549ab3f5884380117
racavalcante <rafael.cavalcante@mercadolivre.com>
2022-08-10 10:21:12 -0300

    docs: add javaDoc annotations

--- src/main/java/dh/meli/projeto_integrador/dto/dtoOutput/CartProductsOutputDto.java
+++ src/main/java/dh/meli/projeto_integrador/dto/dtoOutput/CartProductsOutputDto.java
@@ -2,10 +2,32 @@ package dh.meli.projeto_integrador.dto.dtoOutput;
 
 import lombok.*;
 
-@Getter @Setter
+/**
+ * Method Getter implemented by Lombok lib for get access the private attributes of the Product Class
+ */
+@Getter
+/**
+ * Method Setter implemented by Lombok lib for set the private attributes of the Product Class
+ */
+@Setter
+/**
+ * Method Default Constructor implemented by Lombok lib
+ */
 @NoArgsConstructor
+/**
+ * Method Constructor with all arguments implemented by Lombok lib
+ */
 @AllArgsConstructor
+/**
+ * Method builder implemented by Lombok lib
+ */
 @Builder
+/**
+ * Class used to create a Data Transfer Object for used on CartOutputDto POJO
+ * @author Rafael Cavalcante
+ * @version 0.0.1
+ * @see java.lang.Object
+ */
 public class CartProductsOutputDto {
     private String name;
     private String type;
