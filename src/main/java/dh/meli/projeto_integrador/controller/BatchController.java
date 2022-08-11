90755cb85e4906caddb874b7184d5851e9d15340
diovanavalim <diovana.valim@mercadolivre.com>
2022-08-11 16:05:20 -0300

    feat: add batch controller

---
+++ src/main/java/dh/meli/projeto_integrador/controller/BatchController.java
@@ -0,0 +1,39 @@
+package dh.meli.projeto_integrador.controller;
+
+import dh.meli.projeto_integrador.dto.dtoOutput.BatchStockDto;
+import dh.meli.projeto_integrador.service.BatchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class BatchController {
+
+    @Autowired
+    private BatchService batchService;
+
+    @GetMapping("/fresh-products/due-date/section/{sectionId}/number-of-days/{numberOfDays}")
+    public ResponseEntity<List<BatchStockDto>> getBatchBySectionOrderedByDueDate(@PathVariable long sectionId,
+                                                                    @PathVariable long numberOfDays) {
+        List<BatchStockDto> batchStockDtoList = batchService.getBatchBySectionOrderedByDueDate(sectionId,
+                numberOfDays);
+
+        return new ResponseEntity<List<BatchStockDto>>(batchStockDtoList, HttpStatus.OK);
+    }
+
+    @GetMapping("/fresh-products/due-date/number-of-days/{numberOfDays}/category/{category}")
+    public ResponseEntity<List<BatchStockDto>> getBatchByCategoryOrderedByDueDate(@PathVariable long numberOfDays,
+                                                                                  @PathVariable String category) {
+        List<BatchStockDto> batchStockDtoList = batchService.getBatchByCategoryOrderedByDueDate(numberOfDays,
+                category);
+
+        return new ResponseEntity<List<BatchStockDto>>(batchStockDtoList, HttpStatus.OK);
+    }
+}
