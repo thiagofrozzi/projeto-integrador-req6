1c5704b8cdba595b059ff70ded03b7e56c49b0e9
amarinellif <amanda.marinelli@mercadolivre.com>
2022-08-11 17:19:01 -0300

    chor: add arquivo que gera um ProductStockDto vazio para o teste do controller

--- src/test/java/dh/meli/projeto_integrador/util/Generators.java
+++ src/test/java/dh/meli/projeto_integrador/util/Generators.java
@@ -4,6 +4,7 @@ import dh.meli.projeto_integrador.dto.dtoInput.BatchDto;
 import dh.meli.projeto_integrador.dto.dtoInput.OrderEntryDto;
 import dh.meli.projeto_integrador.dto.dtoInput.SectionDto;
 import dh.meli.projeto_integrador.dto.dtoOutput.ProductOutputDto;
+import dh.meli.projeto_integrador.dto.dtoOutput.ProductStockDto;
 import dh.meli.projeto_integrador.model.*;
 
 import java.time.LocalDate;
@@ -96,79 +97,6 @@ public class Generators {
 
         return batch01;
     }
-    public static Batch createBatch02() {
-        Warehouse warehouse = new Warehouse();
-
-        warehouse.setId(0);
-        warehouse.setName("Armazém 01");
-        warehouse.setAddress("Rua Almeida 259");
-
-        Section section = new Section();
-
-        section.setId(0);
-        section.setCurrentProductLoad(130);
-        section.setMaxProductLoad(2000);
-        section.setProductType("Fresco");
-        section.setWarehouse(warehouse);
-
-        HashSet<Section> sections = new HashSet<Section>();
-
-        Agent agent = new Agent();
-        agent.setId(0);
-        agent.setName("João Maria");
-        agent.setPhoneNumber("48 999343899");
-        agent.setEmailAddress("joaomaria@gmail.com");
-        agent.setWarehouse(warehouse);
-
-        HashSet<Agent> agents = new HashSet<Agent>();
-        agents.add(agent);
-
-        sections.add(section);
-
-        warehouse.setSections(sections);
-        warehouse.setAgents(agents);
-
-        Product product = new Product();
-        product.setPrice(12.20);
-        product.setId(0);
-        product.setName("Maçã");
-        product.setType("Fruta");
-
-        OrderEntry orderEntry = new OrderEntry();
-
-        orderEntry.setId(0);
-        orderEntry.setOrderDate(LocalDate.now());
-        orderEntry.setSection(section);
-
-        HashSet<OrderEntry> orderEntries = new HashSet<OrderEntry>();
-
-        orderEntries.add(orderEntry);
-
-        section.setOrderEntries(orderEntries);
-
-        Batch batch01 = new Batch();
-
-        batch01.setId(0);
-        batch01.setCurrentTemperature(10);
-        batch01.setMinimumTemperature(0);
-        batch01.setInitialQuantity(100);
-        batch01.setCurrentQuantity(80);
-        batch01.setManufacturingDate(LocalDate.now());
-        batch01.setManufacturingTime(LocalTime.now());
-        batch01.setDueDate(LocalDate.now());
-        batch01.setProduct(product);
-        batch01.setOrderEntry(orderEntry);
-
-        HashSet<Batch> batches = new HashSet<Batch>();
-
-        batches.add(batch01);
-
-        orderEntry.setBatches(batches);
-
-        return batch01;
-    }
-
-
 
     public static OrderEntryDto createOrderEntryDto() {
         BatchDto batchDto = new BatchDto();
@@ -543,5 +471,9 @@ public class Generators {
 
     public static List<Batch> emptyBatchList() {
         return new ArrayList<>();
+    }
+
+    public static ProductStockDto getEmptyProductStockDto (){
+        return new ProductStockDto();
     }
 }
