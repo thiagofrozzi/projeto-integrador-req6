9ecb480b019d025bfbd779936fc5ad85978f798b
amarinellif <amanda.marinelli@mercadolivre.com>
2022-08-12 10:41:03 -0300

    feat: inclui o teste de integração do req 3

--- src/test/java/dh/meli/projeto_integrador/integration/ProductIntegrationTest.java
+++ src/test/java/dh/meli/projeto_integrador/integration/ProductIntegrationTest.java
@@ -1,7 +1,9 @@
 package dh.meli.projeto_integrador.integration;
 
-import dh.meli.projeto_integrador.model.Product;
-import dh.meli.projeto_integrador.repository.IProductRepository;
+import dh.meli.projeto_integrador.dto.dtoOutput.ProductStockDto;
+import dh.meli.projeto_integrador.model.*;
+import dh.meli.projeto_integrador.repository.*;
+import dh.meli.projeto_integrador.service.IWarehouseService;
 import dh.meli.projeto_integrador.util.Generators;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,7 +15,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -26,6 +31,21 @@ public class ProductIntegrationTest {
 
     @Autowired
     private IProductRepository productRepository;
+
+    @Autowired
+    private IWarehouseRepository warehouseRepository;
+
+    @Autowired
+    private ISectionRepository sectionReposity;
+
+    @Autowired
+    private IAgentRepository agentRepository;
+
+    @Autowired
+    private IOrderRepository orderRepository;
+
+    @Autowired
+    private IBatchRepository batchRepository;
 
     @Autowired
     private MockMvc mockMvc;
@@ -51,7 +71,8 @@ public class ProductIntegrationTest {
                 .andExpect(jsonPath("$.size()",
                         CoreMatchers.is(list.size())))
                 .andExpect(jsonPath("$[0].name",
-                        CoreMatchers.is(Generators.validProduct1().getName())));;
+                        CoreMatchers.is(Generators.validProduct1().getName())));
+        ;
     }
 
     @Test
@@ -79,12 +100,12 @@ public class ProductIntegrationTest {
 
         ResultActions response = mockMvc.perform(
                 get("/api/v1/fresh-products/{category}", Generators.validProduct2().getType())
-                .contentType(MediaType.APPLICATION_JSON));
+                        .contentType(MediaType.APPLICATION_JSON));
 
 
         response.andExpect(status().isOk())
                 .andExpect(jsonPath("$.size()",
-                        CoreMatchers.is(list.size() -1)))
+                        CoreMatchers.is(list.size() - 1)))
                 .andExpect(jsonPath("$[0].name",
                         CoreMatchers.is(Generators.validProduct2().getName())))
                 .andExpect(jsonPath("$[0].type",
@@ -111,4 +132,38 @@ public class ProductIntegrationTest {
                 .andExpect(jsonPath("$.title",
                         CoreMatchers.is("Object Not Found")));
     }
+
+    @Test
+    public void getProductBratches_ReturnProductStockDto_whenSucess() throws Exception {
+
+        warehouseRepository.save(Generators.createWarehouse());
+
+        sectionReposity.save(Generators.createSectionFresco());
+        sectionReposity.save(Generators.createSectionRefrigerado());
+        sectionReposity.save(Generators.createSectionCongelado());
+
+        agentRepository.save(Generators.createAgent());
+
+        productRepository.save(Generators.getProduct());
+
+        orderRepository.save(Generators.createOrderEntry());
+
+        batchRepository.save(Generators.createBatchDueDate21Days());
+        batchRepository.save(Generators.createBatchDueDate60Days());
+        batchRepository.save(Generators.createBatchDueDate90Days());
+
+        ProductStockDto productStockDto = Generators.getProductStockDtos();
+
+        ResultActions response = mockMvc.perform(get("/api/v1/fresh-products/list/{id}",
+                Generators.getProduct().getId())
+                .contentType(MediaType.APPLICATION_JSON));
+        response.andExpect(status().isOk())
+
+                .andExpect(jsonPath("$.batchStockDto.size()",
+                        CoreMatchers.is(productStockDto.getBatchStockDto().size()-1)))
+                .andExpect(jsonPath("$.name",
+                        CoreMatchers.is(productStockDto.getName())));
+
+    }
+
 }
