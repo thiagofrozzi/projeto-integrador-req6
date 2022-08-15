8824818e863f4bfc2cd19ed33799676b2eecb93d
Thiago Frozzi <thiago.frozzi@mercadolivre.com>
2022-08-15 19:19:09 -0300

    feat: implementa os metodos da interface

---
+++ src/main/java/dh/meli/projeto_integrador/service/CustomerService.java
@@ -0,0 +1,34 @@
+package dh.meli.projeto_integrador.service;
+
+import dh.meli.projeto_integrador.dto.dtoInput.CustomerDto;
+import dh.meli.projeto_integrador.dto.dtoOutput.NewCustomerDto;
+import dh.meli.projeto_integrador.exception.CustomerExistException;
+import dh.meli.projeto_integrador.model.Customer;
+import dh.meli.projeto_integrador.repository.ICustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private ICustomerRepository customerRepository;
+
+    public NewCustomerDto createCustomer(CustomerDto customerDto){
+        Customer findCustomer = customerRepository.findByCpf(customerDto.getCpf());
+
+        if(findCustomer != null){
+            throw new CustomerExistException("This Customer already exist");
+        }
+        Customer customer = Customer.builder()
+                .name(customerDto.getName())
+                .emailAddress(customerDto.getEmail())
+                .phoneNumber(customerDto.getNumber())
+                .cpf(customerDto.getCpf())
+            .build();
+
+        Customer savedCustomer = customerRepository.save(customer);
+
+        return new NewCustomerDto(savedCustomer);
+    }
+}
