f20eb31f71645c644b2447be87cf26bc805aaa7f
Thiago Frozzi <thiago.frozzi@mercadolivre.com>
2022-08-16 12:13:51 -0300

    feat: add rota get customer by id

--- src/main/java/dh/meli/projeto_integrador/controller/CustomerController.java
+++ src/main/java/dh/meli/projeto_integrador/controller/CustomerController.java
@@ -1,20 +1,14 @@
 package dh.meli.projeto_integrador.controller;
 
-import dh.meli.projeto_integrador.dto.dtoInput.CartDto;
 import dh.meli.projeto_integrador.dto.dtoInput.CustomerDto;
+import dh.meli.projeto_integrador.dto.dtoOutput.CustomerOutputDto;
 import dh.meli.projeto_integrador.dto.dtoOutput.NewCustomerDto;
-import dh.meli.projeto_integrador.dto.dtoOutput.TotalPriceDto;
-import dh.meli.projeto_integrador.enumClass.PurchaseOrderStatusEnum;
-import dh.meli.projeto_integrador.exception.ForbiddenException;
+import dh.meli.projeto_integrador.model.Customer;
 import dh.meli.projeto_integrador.service.CustomerService;
-import dh.meli.projeto_integrador.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -29,5 +23,11 @@ public class CustomerController {
     public ResponseEntity<NewCustomerDto> create(@RequestBody @Valid CustomerDto customerDto) {
         NewCustomerDto createdCustomer = customerService.createCustomer(customerDto);
         return new ResponseEntity<>(createdCustomer, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerOutputDto> create(@PathVariable long id) {
+        CustomerOutputDto result = customerService.findById(id);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
