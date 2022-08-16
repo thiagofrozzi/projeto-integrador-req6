0acd37b192dedc4c741c3b3c3cdbe96bfb5e4478
Thiago Frozzi <thiago.frozzi@mercadolivre.com>
2022-08-16 12:14:32 -0300

    feat: add assinatura do metodo findById

--- src/main/java/dh/meli/projeto_integrador/service/ICustomerService.java
+++ src/main/java/dh/meli/projeto_integrador/service/ICustomerService.java
@@ -2,7 +2,10 @@ package dh.meli.projeto_integrador.service;
 
 import dh.meli.projeto_integrador.dto.dtoInput.CustomerDto;
 import dh.meli.projeto_integrador.dto.dtoOutput.NewCustomerDto;
+import dh.meli.projeto_integrador.model.Customer;
 
 public interface ICustomerService {
     NewCustomerDto createCustomer(CustomerDto customerDto);
+
+    Customer findById(long id);
 }
